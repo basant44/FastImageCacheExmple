84cc82a3c21acd26cab1b2e3a630d194fa9d9c55
GS1 <gs1@GS1s-Mac-mini.local>
2015-07-27 12:21:07 +0300

    Initial Commit

---
+++ FastImageCacheExmple/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FastImageCacheExmple
+//
+//  Created by GS1 on 7/27/15.
+//  Copyright (c) 2015 GS1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ FastImageCacheExmple/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FastImageCacheExmple
+//
+//  Created by GS1 on 7/27/15.
+//  Copyright (c) 2015 GS1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
